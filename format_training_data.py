7394fc7a6c9e495a8562bbc0c61314e076e53cc9
stevebottos <steve.bottos@hotmail.ca>
2020-05-02 15:21:07 -0700

    Getting the model to work

--- format_training_data.py
+++ format_training_data.py
@@ -1,41 +1,31 @@
-import os
 import pandas as pd
 import ast
-pd.set_option("display.max_rows", None,
-"display.max_columns", None,
-'display.expand_frame_repr', False,
-'display.max_colwidth', -1)
+import os
 
-"""
-This script just takes the stuff outputted by the vgg image annotator and puts it into a clearer format
-"""
+images = pd.read_csv("vehicles/batch1_formatted.csv")
+img_path = "vehicles/batch1/"
+
+with open('train.txt', 'w') as trainfile:
+    for _, im in images.iterrows():
+        write_str = ""
+
+        f = im["fname"]
+        f_nospaces = f.replace(" ", "")
+        p = img_path + f_nospaces
+        os.rename(img_path + f, p)
 
 
-raw_dat = pd.read_csv("vehicles/batch1_via_region_data.csv")
-im_path = "vehicles/batch1"
-flist = os.listdir(im_path)
+        write_str += p + " "
 
-filename_dat = []
-boxes_dat = []
-for f in flist:
-    boxes = {}
-    image_info = raw_dat.loc[raw_dat["filename"] == f]
+        attributes = ast.literal_eval(im["roi"])
+        bbs = []
+        for key in attributes:
+            rect = attributes[key]
+            x1 = int(rect["xmin"])
+            x2 = int(rect["xmax"])
+            y1 = int(rect["ymin"])
+            y2 = int(rect["ymax"])
 
-    dict_idx = 1
-    for _, row in image_info.iterrows():
-        attributes = ast.literal_eval(row["region_shape_attributes"])
-        boxes[dict_idx] = {}
-        boxes[dict_idx]["xmin"] = attributes["x"]
-        boxes[dict_idx]["ymin"] = attributes["y"]
-        boxes[dict_idx]["xmax"] = attributes["x"] + attributes["width"]
-        boxes[dict_idx]["ymax"] = attributes["y"] + attributes["height"]
-        boxes[dict_idx]["region_name"] = "plate"
-
-        dict_idx += 1
-
-    filename_dat += [f]
-    boxes_dat += [boxes]
-
-row_data = list(zip(filename_dat, boxes_dat))
-formatted = pd.DataFrame(row_data, columns = ["fname", "roi"])
-formatted.to_csv("vehicles/batch1_formatted.csv")
+            write_str += str(x1) + ',' + str(y1) + ',' + str(x2) + ',' + str(y2) + ',' + "0 "
+        print(write_str)
+        trainfile.write(write_str + "\n")
