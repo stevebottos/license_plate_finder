457de9bc02082ffd3ff90ee96ce03d48f57ac1ab
stevebottos <steve.bottos@hotmail.ca>
2020-05-01 23:48:25 -0700

    adding image augmentation

--- img_augmentation.py
+++ img_augmentation.py
@@ -1,51 +1,68 @@
+import imgaug as ia
 import imgaug.augmenters as iaa
 from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 import cv2
 import os
+import pandas as pd
+import ast
+import numpy as np
 
-im_path = "vehicles/batch1"
-flist = os.listdir(im_path)
+"""
+We'll use image augmentation to synthetically add to the dataset
+"""
+
+def aug_pts_to_dict(bbs_aug):
+    boxes = {}
+    dict_idx = 1
+    for i in range(len(bbs_aug)):
+        boxes[dict_idx] = {}
+        boxes[dict_idx]["xmin"] = int(round(bbs_aug[i].x1,0))
+        boxes[dict_idx]["xmax"] = int(round(bbs_aug[i].x2,0))
+        boxes[dict_idx]["ymin"] = int(round(bbs_aug[i].y1,0))
+        boxes[dict_idx]["ymax"] = int(round(bbs_aug[i].y2,0))
+        boxes[dict_idx]["region_name"] = "plate"
+        dict_idx += 1
+    return boxes
 
 
 
-for f in flist:
-    print(f)
+images = pd.read_csv("vehicles/batch1_formatted.csv")
+ims_path = "vehicles/batch1/"
+
+# For new data
+filename_dat = []
+boxes_dat = []
+
+for _, im in images.iterrows():
+    i = cv2.imread(ims_path + im["fname"])
+
+    attributes = ast.literal_eval(im["roi"])
+    bbs = []
+    for key in attributes:
+        rect = attributes[key]
+        x1 = int(rect["xmin"])
+        x2 = int(rect["xmax"])
+        y1 = int(rect["ymin"])
+        y2 = int(rect["ymax"])
+
+        bbs.append(BoundingBox(x1=x1, y1=y1, x2=x2, y2=y2))
+
+    bbs = BoundingBoxesOnImage(bbs, i.shape)
+
+    seq = iaa.Multiply((0.4, 0.5))
+    image_aug, bbs_aug = seq(image=i, bounding_boxes=bbs)
+    dict_pts = aug_pts_to_dict(bbs_aug)
+    print(dict_pts)
 
 
 
 
 
-#
-# ia.seed(1)
-#
-# image = ia.quokka(size=(256, 256))
-# bbs = BoundingBoxesOnImage([
-#     BoundingBox(x1=65, y1=100, x2=200, y2=150),
-#     BoundingBox(x1=150, y1=80, x2=200, y2=130)
-# ], shape=image.shape)
-#
-# seq = iaa.Sequential([
-#     iaa.Multiply((1.2, 1.5)), # change brightness, doesn't affect BBs
-#     iaa.Affine(
-#         translate_px={"x": 40, "y": 60},
-#         scale=(0.5, 0.7)
-#     ) # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
-# ])
-#
-# # Augment BBs and images.
-# image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
-#
-# # print coordinates before/after augmentation (see below)
-# # use .x1_int, .y_int, ... to get integer coordinates
-# for i in range(len(bbs.bounding_boxes)):
-#     before = bbs.bounding_boxes[i]
-#     after = bbs_aug.bounding_boxes[i]
-#     print("BB %d: (%.4f, %.4f, %.4f, %.4f) -> (%.4f, %.4f, %.4f, %.4f)" % (
-#         i,
-#         before.x1, before.y1, before.x2, before.y2,
-#         after.x1, after.y1, after.x2, after.y2)
-#     )
-#
-# # image with BBs before/after augmentation (shown below)
-# image_before = bbs.draw_on_image(image, size=2)
-# image_after = bbs_aug.draw_on_image(image_aug, size=2, color=[0, 0, 255])
+    # image with BBs before/after augmentation (shown below)
+    # image_before = bbs.draw_on_image(i, size=2)
+    # image_after = bbs_aug.draw_on_image(image_aug, size=2, color=[0, 0, 255])
+    #
+    # cv2.imshow("b", image_before)
+    # cv2.imshow("a", image_after)
+    # cv2.waitKey(1000)
+    # cv2.destroyAllWindows()
